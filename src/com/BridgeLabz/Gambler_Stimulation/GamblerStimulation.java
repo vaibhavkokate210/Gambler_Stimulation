2413650d3e995177322b92fedd7c8f776dc9e6de
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-12 12:09:49 +0530

    Gambler is Winning or Lossing completed

--- src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
+++ src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
@@ -2,10 +2,20 @@ package com.BridgeLabz.Gambler_Stimulation;
 
 public class GamblerStimulation 
 {
+	public static final int Stake=100;
+	public static final int Bet=1;
+	public static String WinOrLoss()
+	{
+		double Random=Math.random();
+		if(Random>0.5)
+			return "Winning";
+		else
+			return "Lossing";
+	}
 	public static void main(String[] args)
 	{
-		int Stake=100;
-		int Bet=1;
+		String Result = WinOrLoss();
+		System.out.println("Gambler is "+Result);
 	}
 
 }
