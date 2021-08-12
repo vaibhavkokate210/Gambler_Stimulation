042b3b64c4d9f34f46ca9adad41c6947e6fe541a
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-12 12:58:05 +0530

    gamber win or loss to resign of day

--- src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
+++ src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
@@ -2,26 +2,45 @@ package com.BridgeLabz.Gambler_Stimulation;
 
 public class GamblerStimulation 
 {
-	public static int Stake=100;
-	public static int Bet=1;
-	public static String WinOrLoss(int Bet)
+	public static int stake=100;
+	public static int bet=1;
+	public static String WinOrLoss(int bet)
 	{
 		double random=Math.random();
 		if(random>0.5)
 		{
-			Stake+=Bet;
+			stake+=bet;
 			return "Winning";
 		}
 		else
 		{
-			Stake-=Bet;
+			stake-=bet;
 			return "Lossing";
 		}
 	}
 	public static void main(String[] args)
 	{
-		String result = WinOrLoss(Bet);
-		System.out.println("Gambler is "+result+" "+Bet+" $");
+		//String result = WinOrLoss(bet);
+		//System.out.println("Gambler is "+result+" "+bet+" $");
+		int initStake=stake;
+		while(stake>(initStake*0.5)&&stake<(initStake*1.5))
+		{
+			double random=Math.random();
+			if(random>0.5)
+			{
+				stake+=bet;
+			}
+			else
+			{
+				stake-=bet;
+			}
+		}
+		if(stake>(initStake*0.5))
+		{
+			System.out.println("Gambler loss 50%");
+		}
+		else
+			System.out.println("Gambler gain 50%");
 	}
 
 }
