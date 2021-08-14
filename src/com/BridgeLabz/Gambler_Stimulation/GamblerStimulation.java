ec5a4ae44c54eee02d907067351ba0caa95268f1
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-14 10:47:53 +0530

    [Vaibhav] added After day 20 win or loss

--- src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
+++ src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
@@ -4,9 +4,8 @@ public class GamblerStimulation
 {
 	public static int stake=100;
 	public static int bet=1;
-	public static int initStake=stake;
-    public static final double FIFTY_PERCENT_LOSS=initStake*0.5;
-    public static final double FIFTY_PERCENT_GAIN=stake*1.5;
+	public static int totalAmount=0;
+	
 	public static String WinOrLoss()
 	{
 		double random=Math.random();
@@ -20,30 +19,38 @@ public class GamblerStimulation
 		}
 	}
 	public static void ResignDay()
-	{ 
+	{
 		
-		while(stake>(FIFTY_PERCENT_LOSS)&&stake<(FIFTY_PERCENT_GAIN))
+	    double FIFTY_PERCENT_LOSS=stake*0.5;
+	    double FIFTY_PERCENT_GAIN=stake*1.5;
+		while(stake>(FIFTY_PERCENT_LOSS) && stake<(FIFTY_PERCENT_GAIN))
 		{
 			
 			if(WinOrLoss()=="Winning")
 			{
 				stake+=bet;
+		
 			}
 			else
 			{
 				stake-=bet;
+			
 			}
 		}
 		
 	}
-	
+	public static void DayAmountWinOrLoss()
+	{
+		for(int i=0;i<20;i++)
+			ResignDay();
+		
+	}
 	public static void main(String[] args)
 	{
-		ResignDay();
-		if(stake<100)
-			System.out.println("Gambler loss and total amount = " +stake);
+		DayAmountWinOrLoss();
+		if(stake>100)
+			System.out.println("Gambler win and total amount = " +stake);
 		else
-			System.out.println("Gambler win and total amount ="+stake);
+			System.out.println("Gambler loss and total amount ="+stake);
 	}
-
 }
