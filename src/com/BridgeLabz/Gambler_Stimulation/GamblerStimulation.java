e50e852f2672356740a17f23ef18c921aabca6cd
vaibhavkokate210 <vaibhavkokate210@gmail.com>
2021-08-14 10:51:52 +0530

    [Vaibhav] added Each month how many day win or loss

--- src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
+++ src/com/BridgeLabz/Gambler_Stimulation/GamblerStimulation.java
@@ -18,9 +18,9 @@ public class GamblerStimulation
 			return "Lossing";
 		}
 	}
-	public static void ResignDay()
+	public static void ResignDay(int i)
 	{
-		
+		int initialStackOfDay=stake;
 	    double FIFTY_PERCENT_LOSS=stake*0.5;
 	    double FIFTY_PERCENT_GAIN=stake*1.5;
 		while(stake>(FIFTY_PERCENT_LOSS) && stake<(FIFTY_PERCENT_GAIN))
@@ -37,12 +37,22 @@ public class GamblerStimulation
 			
 			}
 		}
+		if(initialStackOfDay<stake)
+		{
+			int win=stake-initialStackOfDay;
+			System.out.println("Day "+i+" win = "+win);
+		}
+		else
+		{
+			int loss=initialStackOfDay-stake;
+			System.out.println("Day "+i+" loss = "+loss);
+		}
 		
 	}
 	public static void DayAmountWinOrLoss()
 	{
 		for(int i=0;i<20;i++)
-			ResignDay();
+			ResignDay(i+1);
 		
 	}
 	public static void main(String[] args)
@@ -53,4 +63,5 @@ public class GamblerStimulation
 		else
 			System.out.println("Gambler loss and total amount ="+stake);
 	}
+
 }
